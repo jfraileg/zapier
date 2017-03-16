4a0eb5868d61a50bfbf6196b5fd2631efed317a5
jfraileg <jfraileg@everis.com>
2017-03-16 17:11:18 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,108 @@
+'use strict';
+// Module Dependencies
+// -------------------
+var express     = require('express');
+var http        = require('http');
+var JWT         = require('./lib/jwtDecoder');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activityCreate   = require('./routes/activityCreate');
+var activityUpdate   = require('./routes/activityUpdate');
+var activityUtils    = require('./routes/activityUtils');
+var pkgjson = require( './package.json' );
+
+var app = express();
+
+// Register configs for the environments where the app functions
+// , these can be stored in a separate file using a module like config
+
+
+var APIKeys = {
+    appId           : '257f314a-612e-47d3-906c-59db5792dadd',
+    clientId        : 'wv30voswwvztmbx6m62gkper',
+    clientSecret    : 'em7GFq3R80Xm4c6ZOT0jgQXp',
+    appSignature    : 'eygr4tfsr44xrspsdypaggt3j5ev1mfkqq0hle003ka1vshh1pza32uck5flu5npt1nfek3xfxgpjatnux2oa4gwaxohi5uzjbbfb5ayjtkt5p3bgosjxpgzoqz4magyucvzcwdkhas2vercji4wlol3p52kj4eh1lz2ri2bxxahb4jmm5u3swcrm5tldgjnmdvdph4fynwbo1fncyvlpmhw2f3hhn51w5tlwugqgs2dkzn1boejxhp0myyyygf',
+    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
+};
+
+
+// Simple custom middleware
+function tokenFromJWT( req, res, next ) {
+    // Setup the signature for decoding the JWT
+    var jwt = new JWT({appSignature: APIKeys.appSignature});
+    
+    // Object representing the data in the JWT
+    var jwtData = jwt.decode( req );
+
+    // Bolt the data we need to make this call onto the session.
+    // Since the UI for this app is only used as a management console,
+    // we can get away with this. Otherwise, you should use a
+    // persistent storage system and manage tokens properly with
+    // node-fuel
+    req.session.token = jwtData.token;
+    next();
+}
+
+// Use the cookie-based session  middleware
+app.use(express.cookieParser());
+
+// TODO: MaxAge for cookie based on token exp?
+app.use(express.cookieSession({secret: "DeskAPI-CookieSecret0980q8w0r8we09r8"}));
+
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.favicon());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express in Development Mode
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', tokenFromJWT, routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Activity Routes for interacting with Desk.com API
+app.post('/activity/zapier/save/', activityCreate.save );
+app.post('/activity/zapier/validate/', activityCreate.validate );
+app.post('/activity/zapier/publish/', activityCreate.publish );
+app.post('/activity/zapier/execute/', activityCreate.execute );
+
+
+app.get('/clearList', function( req, res ) {
+	// The client makes this request to get the data
+	activityUtils.logExecuteData = [];
+	res.send( 200 );
+});
+
+
+// Used to populate events which have reached the activity in the interaction we created
+app.get('/getActivityData', function( req, res ) {
+	// The client makes this request to get the data
+	if( !activityUtils.logExecuteData.length ) {
+		res.send( 200, {data: null} );
+	} else {
+		res.send( 200, {data: activityUtils.logExecuteData} );
+	}
+});
+
+app.get( '/version', function( req, res ) {
+	res.setHeader( 'content-type', 'application/json' );
+	res.send(200, JSON.stringify( {
+		version: pkgjson.version
+	} ) );
+} );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
