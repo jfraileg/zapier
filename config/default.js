5389805d02314223fcee15d44b1d3b6b17fc0e51
jfraileg <jfraileg@everis.com>
2017-03-17 10:16:21 +0100

    Update default.js

--- config/default.js
+++ config/default.js
@@ -3,5 +3,5 @@ module.exports = {
    clientId        : 'wv30voswwvztmbx6m62gkper',
    clientSecret    : 'em7GFq3R80Xm4c6ZOT0jgQXp',
    appSignature    : 'eygr4tfsr44xrspsdypaggt3j5ev1mfkqq0hle003ka1vshh1pza32uck5flu5npt1nfek3xfxgpjatnux2oa4gwaxohi5uzjbbfb5ayjtkt5p3bgosjxpgzoqz4magyucvzcwdkhas2vercji4wlol3p52kj4eh1lz2ri2bxxahb4jmm5u3swcrm5tldgjnmdvdph4fynwbo1fncyvlpmhw2f3hhn51w5tlwugqgs2dkzn1boejxhp0myyyygf',
-   authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
+   authUrl         : 'https://auth-test.exacttargetapis.com/v1/requestToken?legacy=1'
 };
